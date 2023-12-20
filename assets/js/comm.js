e5c47b3264bbb219412604a297d4f08978430002
hyeeons <qouop0101@gmail.com>
2023-12-20 18:04:32 +0900

    html 수정

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,27 +1,57 @@
-var swiper = new Swiper(".slide-bnr", {
+var swiper = new Swiper(".swiper-bnr", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+});
+
+var swiper = new Swiper(".swiper-history", {
+  direction: "vertical",
+  slidesPerView: "auto",
+  freeMode: true,
+  scrollbar: {
+    el: ".swiper-scrollbar",
   },
+  mousewheel: true,
+});
+
+var swiper = new Swiper(".swiper-prod", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // 768: {
+    //   slidesPerView: 4,
+    //   spaceBetween: 40,
+    // },
+    // 1280: {
+    //   slidesPerView: 5,
+    //   spaceBetween: 50,
+    // },
   },
 });
 
-var swiper = new Swiper(".slide-vdo", {
+var swiper = new Swiper(".swiper-vdo", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
   },
 });
