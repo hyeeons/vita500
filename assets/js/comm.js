b7ba8ed2ff4a5cf2b1d46c881fb23c96d97d782c
hyeeons <qouop0101@gmail.com>
2023-12-23 21:27:47 +0900

    main

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -18,32 +18,33 @@ var swiper = new Swiper(".swiper-history", {
   freeMode: true,
   scrollbar: {
     el: ".swiper-scrollbar",
+    hide: true,
   },
   mousewheel: true,
 });
 
 var swiper = new Swiper(".swiper-prod", {
-  slidesPerView: 1,
-  spaceBetween: 30,
+  slidesPerView: 2,
+  spaceBetween: 33,
   loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    // 768: {
-    //   slidesPerView: 4,
-    //   spaceBetween: 40,
-    // },
-    // 1280: {
-    //   slidesPerView: 5,
-    //   spaceBetween: 50,
-    // },
-  },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  //   breakpoints: {
+  //     640: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 20,
+  //     },
+  //     // 768: {
+  //     //   slidesPerView: 4,
+  //     //   spaceBetween: 40,
+  //     // },
+  //     // 1280: {
+  //     //   slidesPerView: 5,
+  //     //   spaceBetween: 50,
+  //     // },
+  //   },
 });
 
 var swiper = new Swiper(".swiper-vdo", {
